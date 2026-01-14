e5d6d6c2bb7840ead4d4f7bf8d27f1e3520fd9e7
Anthony Ruiz <anthonyruiz20048@gmail.com>
2026-01-14 09:51:24 -0500

    Solución de carpeta vacía y subida de archivos de la app

---
+++ app04/superbase/Config.tsx
@@ -0,0 +1,4 @@
+import { createClient } from '@supabase/supabase-js'
+// Create a single supabase client for interacting with your database
+export const supabase = createClient('https://mrbnlxpxfkueosktlcpd.supabase.co', 
+    'sb_publishable_rNk3HrhscNQPT1ugAtq8CA_BaXzUUNy')
